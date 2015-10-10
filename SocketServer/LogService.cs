aec8552be84ecc1bb9feceeaa31ed468a781c6b9
YangXin <yangxinhust@hotmail.com>
2015-10-10 16:41:53 +0800

    Add LogService to SocketServer

---
+++ SocketServer/LogService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SocketServer
+{
+    public class LogService
+    {
+        private static string logFileLocation = "E:\\ServerLog\\";
+        public static void Log(Message.Message data)
+        {
+            try
+            {
+                StreamWriter sw = File.AppendText(logFileLocation + "log.txt");
+                string temp = "Type" + data.messageType.ToString() + " Time:" + data.time + " User:" + data.userName + " Room:" + data.room + " Text:" + data.text;
+                sw.WriteLine(temp);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+        }
+        public static void LogError(string error)
+        {
+            try
+            {
+                StreamWriter sw = File.AppendText(logFileLocation + "error.txt");
+                sw.WriteLine(System.DateTime.Now.ToString() + "  " + error);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+
+
+        }
+    }
+}
